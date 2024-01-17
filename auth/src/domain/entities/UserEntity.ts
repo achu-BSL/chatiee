372b79fa4bb3f5ef4874fe84aedcbd74ed932a35
achu-bsl <asharudeenalikt@gmail.com>
2024-01-17 08:35:46 +0530

    feat(user entity)

---
+++ auth/src/domain/entities/UserEntity.ts
@@ -0,0 +1,10 @@
+import { randomUUID } from "crypto";
+
+export class User {
+  constructor(
+    public username: string,
+    public email: string,
+    public password: string,
+    public userId = randomUUID()
+  ) {}
+}
