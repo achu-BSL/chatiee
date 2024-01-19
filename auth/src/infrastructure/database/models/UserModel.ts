1ab182ce545774cdbd96dc592e148b3eb32788d4
achu-bsl <asharudeenalikt@gmail.com>
2024-01-19 08:12:30 +0530

    refactor(user repository): added logic

--- auth/src/infrastructure/database/models/UserModel.ts
+++ auth/src/infrastructure/database/models/UserModel.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Model({
+const userSchema = new mongoose.Schema({
     username: {
         type: String,
         unique: true,
--- auth/src/infrastructure/repositories/UserRepository.ts
+++ auth/src/infrastructure/repositories/UserRepository.ts
@@ -1,17 +1,38 @@
 import { IUserRepository } from "../../application/interface/IUserRepository";
 import { User } from "../../domain/entities/UserEntity";
+import userModel from "../database/models/UserModel";
+import bcrypt from "bcrypt";
 
 export class UserRepository implements IUserRepository {
-    async save (user: User) {
-        //save logic
-        return user;
-    }
+  async save(user: User) {
+    const hashedPassword = bcrypt.hash(user.password, 10);
+    const { username, email, _id } = await userModel.create({
+      ...user,
+      password: hashedPassword,
+    });
+    const mongoUser = new User(username, email, "x", _id.toString());
+    return mongoUser;
+  }
 
-    async findUserByEmail(email: string) {
-        return null;
-    }
+  async findUserByEmail(email: string) {
+    const mongoUser = await userModel.findOne({ email });
+    if (!mongoUser) return null;
+    return new User(
+      mongoUser.username,
+      mongoUser.email,
+      "x",
+      mongoUser._id.toString()
+    );
+  }
 
-    async findUserByUsername (username: string) {
-        return null;
-    }
+  async findUserByUsername(username: string) {
+    const mongoUser = await userModel.findOne({ username });
+    if (!mongoUser) return null;
+    return new User(
+      mongoUser.username,
+      mongoUser.email,
+      "x",
+      mongoUser._id.toString()
+    );
+  }
 }
