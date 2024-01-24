c548cf05547033b4089074a9de7fb58008a431c2
achu-BSL <asharudeenalikt@gmail.com>
2024-01-24 08:57:33 +0530

    feat(personal chat mongo yaml file)

--- personalChat/src/main.ts
+++ personalChat/src/main.ts
@@ -1,6 +1,8 @@
+import connectDb from "./infrastructure/database/setup";
 import { Server } from "./presentation/Server"
 
 const main = async () => {
+    await connectDb();
     Server.run(3001);
 };
 
